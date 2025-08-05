a247c6d5ba285a6eb16104374b850a228898d457
Bhanupratap83 <bp747444@gmail.com>
2025-08-05 17:45:26 +0530

    Push Code

---
+++ Backend/src/app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import {createServer} from 'node:http';
+
+import {Server} from 'socket.io';
+
+import mongoose from 'mongoose';
+import {connectToSocket} from './controllers/socketManager.js';
+
+
+import cors from 'cors';
+import userRoutes from './routes/users.routes.js';
+
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({extended: true, limit: "40kb"}));
+
+// app.get('/home', (req, res) => {
+//     return res.json({"hello": "world"});
+// });
+
+app.use('/api/v1/users', userRoutes);
+
+const start = async () => {
+    const connectionDb = await mongoose.connect('mongodb+srv://bp747444:pLA67Zjjk52Uk7IY@cluster0.rgk8nm.mongodb.net/');
+    
+    console.log(`Database connected: ${connectionDb.connection.host}`);
+    app.listen(8000, () => {
+        console.log('Server is running on port 8000');
+    });
+}
+
+start();
---
+++ Frontend/src/pages/landing.jsx
@@ -0,0 +1,43 @@
+import React from "react";  
+import "../App.css";
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function LandingPage() {
+
+    const router = useNavigate();
+    return (
+        <div className="landingPageContainer">
+            <nav>
+                <div className="navHeader">
+                    <h2>Chatzy</h2>
+                </div>
+                <div className="navList">
+                    <p onClick={() => {
+                        router("/cwec");
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth");
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth");
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{color:"#FF9839"}}>Connect</span> with your loved ones</h1>
+                    <p>Cover a distance by Chatzy</p>
+                    <div role="button">
+                        <Link to={"/auth"}>Get Started &rArr;</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src="/mobile.png" alt="facetime"></img>
+                </div>
+            </div>
+        </div>
+    );
+}
